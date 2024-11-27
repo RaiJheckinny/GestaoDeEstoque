8fd653d2c24b73917c250e487424389b1f899e13
RaiJheckinny <raijheckinnymartins@gmail.com>
2024-11-27 20:23:20 -0300

    firt: Configurando projeto

---
+++ src/main/java/br/com/gestaestoque/GestaDeEstoqueApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaDeEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gestaestoque/GestaDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
